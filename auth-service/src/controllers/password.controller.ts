2cb38af41d04cd5fa8ac690497fff852fa4c60ed
NithinKS007 <nithinksndkm@gmail.com>
2026-04-07 11:52:42 +0530

    removed common eslintrc & pretierrc and added it into different service v5

--- auth-service/src/controllers/password.controller.ts
+++ auth-service/src/controllers/password.controller.ts
@@ -1,6 +1,7 @@
 import { PasswordService } from "./../services/password.service";
 import { AuthReq, sendResponse, StatusCodes, validateDto } from "../../../utils/src";
 import { Request, Response } from "express";
+import { ValidationError } from "../../../utils/src/error.handling.middleware";
 import {
   ChangePasswordRequestDto,
   ResetPasswordRequestDto,
@@ -29,8 +30,8 @@ export class PasswordController {
   }
 
   async changePass(req: AuthReq, res: Response): Promise<void> {
-    const data = await validateDto(ChangePasswordRequestDto, req.body);
-    await this.passwordService.changePass({ ...data, userId: req?.user?.id! });
+    const data = await validateDto(ChangePasswordRequestDto, {...req.body,userId:req.user?.id});
+    await this.passwordService.changePass({ ...data, userId:data.userId });
     sendResponse(res, StatusCodes.OK, null, "Password changed successfully");
   }
 }
--- auth-service/src/entity/user.entity.ts
+++ auth-service/src/entity/user.entity.ts
@@ -2,7 +2,7 @@ export interface UserEntity {
   id: string;
   name: string;
   email: string;
-  role: "ADMIN" | "USER" 
+  role: "ADMIN" | "USER";
   password: string;
   createdAt?: Date | undefined;
   updatedAt?: Date | undefined;
